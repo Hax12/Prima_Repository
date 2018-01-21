fbe7c0c5bde25a08734db29dd42ab212d3856821
“Hassen <hassendj12@gmail.com>
2018-01-21 17:39:16 +0100

    Versione 1

---
+++ EsercizioP1/EsercizioP1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsercizioP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ciao Mondo
+            Console.WriteLine("Hello World");
+        }
+    }
+}
